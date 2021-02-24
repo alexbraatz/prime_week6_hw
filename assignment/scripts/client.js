1189b9ee7092694be2d62649c9485ea5fdfdba52
alex braatz <alexjbraatz@gmail.com>
2021-02-24 14:53:12 -0600

    completed main garage goals

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -2,20 +2,34 @@ $( document ).ready( readyNow );
 
 let garage = [];
 
-function readyNow(){
-  let el = $( '#garageTitle' );
+function displayCars(){
+  console.log( 'in displayCars' );
+
+  let el = $( '#cars');
   el.empty();
-  el.append( garage );
-  $( '#addCarButton' ).on( 'click', addCar );
+  for ( i in garage ){
+    el.append( `<li> ${garage[i].year} ${garage[i].make} ${garage[i].model} </li>`);
+  }
 }
 
 function addCar(){
   console.log( 'in addCar' );
 
-  $( '#addCarButton' ).on( 'click', newCar( $('#carYear').val(), $('#carMake').val() , $('#carModel').val() ) );
+  $( '#addCarButton' ).on( 'click', newCar( $( '#carYear' ).val(),
+    $( '#carMake' ).val(), $( '#carModel' ).val() ) );
+    
   $( '#carYear'  ).val('');
   $( '#carMake'  ).val('');
   $( '#carModel' ).val('');
+
+  displayCars();
+}
+
+function readyNow(){
+  let el = $( '#garageTitle' );
+  el.empty();
+  el.append( garage );
+  $( '#addCarButton' ).on( 'click', addCar );
 }
 
 /*
