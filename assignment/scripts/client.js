e1bfc76508cc343be08b7ae2b910c84fcd43df08
alex braatz <alexjbraatz@gmail.com>
2021-02-26 14:46:55 -0600

    added max spaces functionality in garage

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -12,18 +12,30 @@ function displayCars(){
   }
 }
 
+function isGarageFull(){
+  if ( garage.length === 3 ){
+    return true
+  }
+}
+
 function addCar(){
   console.log( 'in addCar' );
 
-  // logic gate to check if any fields are left blank
-  if ( $('#carYear').val().length === 0 || $('#carMake').val().length === 0 || $('#carModel').val().length === 0 ) {
-    console.log( "Oops, missing a field! Not able to add to garage yet. :c");
-    console.log( "Please enter value in all entries");
+  // logic to check if you can add more cars to garage. If it's full,
+  // addCar function will be disabled
+  if( isGarageFull() ) {
+    console.log( "Garage is full! No more cars.")
   } else {
-    $( '#addCarButton' ).on( 'click',
-    newCar( $( '#carYear' ).val(), $( '#carMake' ).val(), $( '#carModel' ).val() )
-  ); // all fields are entered? car is added to the garage with newCar function
-  } // end logic gate
+    // logic gate to check if any fields are left blank
+    if ( $('#carYear').val().length === 0 || $('#carMake').val().length === 0 || $('#carModel').val().length === 0 ) {
+      console.log( "Oops, missing a field! Not able to add to garage yet :c");
+      console.log( "Please enter value in all entries");
+    } else {
+      $( '#addCarButton' ).on( 'click',
+      newCar( $( '#carYear' ).val(), $( '#carMake' ).val(), $( '#carModel' ).val() )
+    ); // all fields are entered? car is added to the garage with newCar function
+    } // end logic gate
+  }
 
   // clear values of input boxes
   $( '#carYear'  ).val('');
