7bb79fcb1a3b01b9b7b3c6f62734741302298b16
alex braatz <alexjbraatz@gmail.com>
2021-02-26 14:39:13 -0600

    complete logic gate to make sure all fields are entered

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -8,25 +8,34 @@ function displayCars(){
   let el = $( '#cars');
   el.empty();
   for ( i in garage ){
-    el.append( `<li> ${garage[i].year}, ${garage[i].make}: ${garage[i].model} </li>`);
+    el.append( `<li> ${garage[i].year} ${garage[i].make}\: ${garage[i].model} </li>`);
   }
 }
 
 function addCar(){
   console.log( 'in addCar' );
 
-  $( '#addCarButton' ).on( 'click', newCar( $( '#carYear' ).val(),
-    $( '#carMake' ).val(), $( '#carModel' ).val() ) );
+  // logic gate to check if any fields are left blank
+  if ( $('#carYear').val().length === 0 || $('#carMake').val().length === 0 || $('#carModel').val().length === 0 ) {
+    console.log( "Oops, missing a field! Not able to add to garage yet. :c");
+    console.log( "Please enter value in all entries");
+  } else {
+    $( '#addCarButton' ).on( 'click',
+    newCar( $( '#carYear' ).val(), $( '#carMake' ).val(), $( '#carModel' ).val() )
+  ); // all fields are entered? car is added to the garage with newCar function
+  } // end logic gate
 
+  // clear values of input boxes
   $( '#carYear'  ).val('');
   $( '#carMake'  ).val('');
   $( '#carModel' ).val('');
 
+  // display cars in garage to DOM
   displayCars();
 }
 
 function readyNow(){
-  let el = $( '#garageTitle' );
+  let el = $( '#garage' );
   el.empty();
   el.append( garage );
   $( '#addCarButton' ).on( 'click', addCar );
