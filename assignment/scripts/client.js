6ac9d09beec786ece42819ebf50ebfa491b81bbb
alex braatz <alexjbraatz@gmail.com>
2021-02-26 14:54:37 -0600

    added comments

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -2,6 +2,7 @@ $( document ).ready( readyNow );
 
 let garage = [];
 
+// begin displayCars
 function displayCars(){
   console.log( 'in displayCars' );
 
@@ -10,14 +11,16 @@ function displayCars(){
   for ( i in garage ){
     el.append( `<li> ${garage[i].year} ${garage[i].make}\: ${garage[i].model} </li>`);
   }
-}
+} // end displayCars
 
+// begin isGarageFull
 function isGarageFull(){
   if ( garage.length === 3 ){
     return true
   }
-}
+} // end isGarageFull
 
+// begin addCar
 function addCar(){
   console.log( 'in addCar' );
 
@@ -27,31 +30,31 @@ function addCar(){
     console.log( "Garage is full! No more cars.")
   } else {
     // logic gate to check if any fields are left blank
-    if ( $('#carYear').val().length === 0 || $('#carMake').val().length === 0 || $('#carModel').val().length === 0 ) {
+    if ( ( $('#carYear').val().length === 0 || $('#carMake').val().length === 0 )
+        || $('#carModel').val().length === 0 ) {
       console.log( "Oops, missing a field! Not able to add to garage yet :c");
       console.log( "Please enter value in all entries");
     } else {
       $( '#addCarButton' ).on( 'click',
-      newCar( $( '#carYear' ).val(), $( '#carMake' ).val(), $( '#carModel' ).val() )
-    ); // all fields are entered? car is added to the garage with newCar function
-    } // end logic gate
-  }
-
-  // clear values of input boxes
-  $( '#carYear'  ).val('');
-  $( '#carMake'  ).val('');
-  $( '#carModel' ).val('');
-
-  // display cars in garage to DOM
+      newCar( $( '#carYear' ).val(), $( '#carMake' ).val(), $( '#carModel' ).val() ));
+      // all fields are entered? car is added to the garage with newCar function
+      $( '#carYear'  ).val('');
+      $( '#carMake'  ).val('');
+      $( '#carModel' ).val('');
+      // clear values of input boxes
+    } // end inner if logic gate
+  } // end outer if logic
   displayCars();
-}
+  // display cars in garage to DOM
+} // end addCar
 
+// begin readyNow function
 function readyNow(){
   let el = $( '#garage' );
   el.empty();
   el.append( garage );
   $( '#addCarButton' ).on( 'click', addCar );
-}
+} // end readyNow function
 
 /*
 Do not change newCar for base mode!
